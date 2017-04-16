4ee2c123ff3d4f3d65fa9f80a28a8c5db5499201
Anastasiia Nedbailo <anastas11a.d3v@gmail.com>
2017-04-16 13:04:21 +0300

    'insert'

--- app/js/database.js
+++ app/js/database.js
@@ -2,47 +2,65 @@ const CONNECTION_STRING = "pg://postgres:er@dmin_2k17@ecoresource.ddns.net:5051/
 
 var pg = require("pg");
 var client = new pg.Client(CONNECTION_STRING);
-var query = client.query("SELECT ST_AsGeoJSON(geom) FROM pointrivermodel limit 10");
+//var query = client.query("SELECT ST_AsGeoJSON(geom) FROM pointrivermodel limit 10");
+// var query = client.query("SELECT gid FROM pointrivermodel");
 var count = 0, val = [];
 
-function insertIntoDB(){
-    for ( var i=0; i < 20;  i++)
-    {
-        val.push(Math.random());
-        /*    client.query(
-         'INSERT INTO test_water (water_polution, cleaned_water) VALUES($1, $2)', [Math.random(), 0],
-         function(err, result) {
-         if (err) {
-         console.log(err);
-         } else {
-         console.log('row inserted with id: ' + result.rows[0].id);
-         }
+function insertIntoDB() {
+    var query = client.query("SELECT gid FROM pointrivermodel");
+    var idArray = [];
+    data.rows.forEach(row => {
+        idArray.push(row.gid);
+    });
 
-         count++;
-         console.log('count = ' + count);
-         if (count == 20) {
-         console.log('Client will end now!!!');
-         client.end();
-         }
-         });*/
+
+    for (var i = 0, arrayLength = Math.ceil(data.rowCount / 500); i < arrayLength; i++) {
+        var dataArray = [];
+        for (var j = 0; j < 500 * (i + 1); j++) {
+            // val.push(Math.random());
+            var index = (i == 0) ? j : j + (i * 500);
+
+            dataArray.push([idArray[index], (0.5 + Math.random() * 101)]);
+        }
+        client.query(
+            'INSERT INTO  pointrivermodel (gid, pollution_in_point) VALUES $1', Inserts('$1, $2', dataArray),
+            function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('row inserted with id: ' + result.rows[0]);
+                }
+
+                count++;
+                console.log('count = ' + count);
+                if (count == arrayLength) {
+                    console.log('Client will end now!!!');
+                    client.end();
+                }
+            });
     }
 }
 
-function selectFromDB(){
+function selectFromDB() {
     return "SELECT ST_AsGeoJSON(geom) FROM pointrivermodel limit 10";
 }
 
+
 client.connect();
 query.on("end", (data) => {
-    let x = new Array();
-    let y = new Array();
-
-    data.rows.forEach(row => {
-        x.push(JSON.parse(row.st_asgeojson).coordinates[0]);
-        y.push(JSON.parse(row.st_asgeojson).coordinates[1]);
-    });
-
-    console.log(x);
-    console.log(y);
+    // let x = new Array();
+    // let y = new Array();
+    //
+    // data.rows.forEach(row => {
+    //     x.push(JSON.parse(row.st_asgeojson).coordinates[0]);
+    //     y.push(JSON.parse(row.st_asgeojson).coordinates[1]);
+    // });
+    // console.log(x);
+    // console.log(y);
+    // var idArray = [];
+    // data.rows.forEach(row => {
+    //     idArray.push(row.gid);
+    // });
+    // console.log(idArray);
     client.end();
 });
